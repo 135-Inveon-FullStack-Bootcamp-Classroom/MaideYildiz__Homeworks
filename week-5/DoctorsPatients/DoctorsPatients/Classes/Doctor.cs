b3528e21d2d5c00cd31ef5b0b65e9cad5f3a5173
Maide Yıldız <maideyildizz@gmail.com>
2021-11-20 02:15:54 +0300

    week 5

---
+++ week-5/DoctorsPatients/DoctorsPatients/Classes/Doctor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoctorsPatients.Classes
+{
+    public class Doctor:Human
+    {
+        public int doctorId { get; set; }
+        public string field { get; set; }
+
+        public override void patientOrDoctor()
+        {
+            Console.WriteLine("This is a doctor");
+        }
+    }
+}
---
+++ week-5/DoctorsPatients/DoctorsPatients/Program.cs
@@ -0,0 +1,69 @@
+﻿using DoctorsPatients.Classes;
+
+var doctor = new Doctor()
+{
+    name = "Ayse",
+    surname = "Bir",
+    field = "KBB",
+    age = 30
+};
+var doctor2 = new Doctor()
+{
+    name = "Fatma",
+    surname = "İki",
+    field = "Goz",
+    age = 25
+};
+var doctor3 = new Doctor()
+{
+    name = "Melek",
+    surname = "Üç",
+    field = "Ortopedi",
+    age = 50
+};
+
+var hasta = new Patient()
+{
+    name = "Reyhan",
+    surname = "Bir",
+    disease = "Kulak",
+    age = 40
+};
+var hasta2 = new Patient()
+{
+    name = "Sultan",
+    surname = "İki",
+    disease = "Goz",
+    age = 35
+};
+var hasta3 = new Patient()
+{
+    name = "Gunes",
+    surname = "Üç",
+    disease = "Ortopedi",
+    age = 15
+};
+
+void displayD(Doctor d)
+{
+    Console.WriteLine("Doctor name, surname: " + d.name + " " + d.surname);
+    Console.WriteLine("Doctor age: " + d.age);
+    Console.WriteLine("Doctor field: " + d.field);
+    Console.WriteLine("--------------------------------");
+}
+void displayP(Patient p)
+{
+    Console.WriteLine("Patient name, surname: " + p.name + " " + p.surname);
+    Console.WriteLine("Patient age: " + p.age);
+    Console.WriteLine("Patient disease: " + p.disease);
+    Console.WriteLine("+++++++++++++++++++++++++++++++++");
+}
+Console.WriteLine("Doctors....");
+displayD(doctor);
+displayD(doctor2);
+displayD(doctor3);
+Console.WriteLine("**********************************");
+Console.WriteLine("Patients....");
+displayP(hasta);
+displayP(hasta2);
+displayP(hasta3);
